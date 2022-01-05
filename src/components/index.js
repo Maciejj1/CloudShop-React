f600c72abe9b6218ebf12529f3e1e7dafeb6ebda
Maciejj1 <maciekwawryszuk@gmail.com>
2022-01-05 14:52:38 +0100

    its work

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,4 @@
 export {default as Navbar} from './NavBar/Navbar'
 export {default as Products} from './Products/Products'
+export {default as Home} from './HomePage/Home'
+export {default as AddProducts} from './AddProducts/AddProducts'
