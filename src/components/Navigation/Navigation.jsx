357f4a7c6dc6a770333f1ba81a8e3952619b1c33
Maciej Wawryszuk <89411648+Maciejj1@users.noreply.github.com>
2022-02-26 22:05:37 +0100

    :D

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -8,50 +8,49 @@ import AddIcon from "@mui/icons-material/Add";
 const Navigation = () => {
   const location = useLocation();
 
-  if (location.pathname === "/")
-    return (
-      <div>
-        <div className="buttton">
+  return (
+    <div>
+      <div className="buttton">
+        <button
+          component={Link}
+          to="/Cart"
+          className="IconButton"
+          aria-label="Show-cart-items"
+        >
+          <Badge className="Badge" badgeContent={2} color="secondary">
+            <ShoppingCart />
+          </Badge>
+        </button>
+        {location.pathname === "/" && (
           <button
             component={Link}
-            to="/Cart"
-            className="IconButton"
-            aria-label="Show-cart-items"
+            to="/"
+            className="HomeButton"
+            aria-label="Home-page"
           >
-            <Badge className="Badge" badgeContent={2} color="secondary">
-              <ShoppingCart />
-            </Badge>
-          </button>
-          {location.pathname === "/" && (
-            <button
-              component={Link}
-              to="/"
-              className="HomeButton"
-              aria-label="Home-page"
-            >
-              <div className="Home">
-                <Home />
-              </div>
-            </button>
-          )}
-          <button
-            component={Link}
-            to="/Login"
-            type="button"
-            className="LoginButton"
-          >
-            <div className="Login">
-              <LoginIcon></LoginIcon>
+            <div className="Home">
+              <Home />
             </div>
           </button>
-          <div className="Add">
-            <Link to="/addproducts">
-              <AddIcon />
-            </Link>
+        )}
+        <button
+          component={Link}
+          to="/Login"
+          type="button"
+          className="LoginButton"
+        >
+          <div className="Login">
+            <LoginIcon />
           </div>
+        </button>
+        <div className="Add">
+          <Link to="/addproducts">
+            <AddIcon />
+          </Link>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Navigation;
